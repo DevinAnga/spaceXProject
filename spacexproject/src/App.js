a97b5468f9794abcd26926c945b53913058a167a
DevinAnga <DevinAnga.dev@Gmail.com>
2020-08-11 04:34:46 -0400

    added basic api for spacex and printed basic info to screen

---
+++ spacexproject/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MyComponent from "./js/ajax.js";
+
+function App() {
+  return (
+    <div className="App">
+      {MyComponent}
+    </div>
+    
+  );
+}
+
+export default App;
